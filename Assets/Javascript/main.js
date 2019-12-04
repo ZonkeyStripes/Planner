af5e0a8e3cc0671a40ec411f54b42012b59cc5b4
Harley <Zonkeystripes@hotmail.com>
2019-12-03 18:11:21 -0700

    tring the moment api. not working.

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -9,6 +9,27 @@ let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oc
 let monthAndYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
 
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timeDisplay').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+startTime();
 
 function next() {
     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
